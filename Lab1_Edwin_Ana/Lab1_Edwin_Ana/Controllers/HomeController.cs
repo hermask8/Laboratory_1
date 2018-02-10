6df6617c7e8f26104745c4314ecc069c3d5ae1e4
EDWIN LEONARDO HILARIO GOMEZ <elhilario@correo.url.edu.gt>
2018-02-10 14:46:00 +0100

    segunda parte ´

--- Lab1_Edwin_Ana/Lab1_Edwin_Ana/Controllers/HomeController.cs
+++ Lab1_Edwin_Ana/Lab1_Edwin_Ana/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Lab1_Edwin_Ana.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using directorios = System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,7 +13,45 @@ namespace Lab1_Edwin_Ana.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return View(new List<Jugador>());
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase archivoBase)
+        {
+            List<Jugador> jugadores = new List<Jugador>();
+            string pathArchivo = string.Empty;
+            if (archivoBase != null)
+            {
+                string path = Server.MapPath("~/Cargas/");
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                pathArchivo = path + Path.GetFileName(archivoBase.FileName);
+                string extension = Path.GetExtension(archivoBase.FileName);
+                archivoBase.SaveAs(pathArchivo);
+                Random miRandom = new Random();
+                string archivoCsv = directorios.File.ReadAllText(pathArchivo);
+                foreach (string lineas in archivoCsv.Split('\n'))
+                {
+                    if (!string.IsNullOrEmpty(lineas))
+                    {
+                        jugadores.Add(new Jugador
+                        {
+                            Club = Convert.ToString(lineas.Split(',')[0]),
+                            Apellido = Convert.ToString(lineas.Split(',')[1]) ,
+                            Nombre = Convert.ToString(lineas.Split(',')[2]) ,
+                            Posicion = Convert.ToString(lineas.Split(',')[3]) ,
+                            Salario = (lineas.Split(',')[4]),
+                            Compensaciones = (lineas.Split(',')[5])
+                        });
+                    }
+                }
+
+            }
+            return View(jugadores);
         }
 
         public ActionResult About()
